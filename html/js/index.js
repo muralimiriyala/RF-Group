c00007a14ef3a5e15bf048346fda1d143992bea8
MuraliJbk <murali@jbkinfotech.com>
2024-12-26 17:31:02 +0530

    selectBox css

--- html/js/index.js
+++ html/js/index.js
@@ -1,7 +1,10 @@
+import { jqueryselectBox } from './lib/jquery.selectBox.js';
+
 import { menu } from './modules/menu';
 import { search } from './modules/search';
 import { slick } from './modules/customSlick';
 import { share } from './modules/share';
+import { selectBox } from './modules/selectBox';
 import { magnificPopup } from './modules/magnificPopup';
 
 (function () {
@@ -9,5 +12,7 @@ import { magnificPopup } from './modules/magnificPopup';
   search.init();
   slick.init();
   share.init();
+  jqueryselectBox.init();
+  selectBox.init();
   magnificPopup.init();
 })();
---
+++ html/js/modules/selectBox.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+
+export const selectBox = {
+  init() {
+    jQuery('select').selectBox({
+      keepInViewport: false,
+      menuSpeed: 'normal',
+      mobile: true,
+      hideOnWindowScroll: true,
+      menuTransition: 'slide',
+    });
+    jQuery('.selectBox, .selectBox-dropdown .selectBox-label').removeAttr(
+      'style'
+    );
+  },
+};
