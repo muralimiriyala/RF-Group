cfbf01c1777862a9d03ddcd02ea1241e5367036e
MuraliJbk <murali@jbkinfotech.com>
2024-12-26 18:51:24 +0530

    selectBox css

--- html/js/index.js
+++ html/js/index.js
@@ -4,6 +4,7 @@ import { jqueryselectBox } from './lib/jquery.selectBox.js';
 
 import { menu } from './modules/menu';
 import { search } from './modules/search';
+import { iframe } from './modules/iframe.js';
 import { slick } from './modules/customSlick';
 import { share } from './modules/share';
 import { selectBox } from './modules/selectBox';
@@ -12,6 +13,7 @@ import { magnificPopup } from './modules/magnificPopup';
 (function () {
   menu.init();
   search.init();
+  iframe.init();
   slick.init();
   share.init();
   jqueryselectBox.init();
---
+++ html/js/modules/iframe.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+import 'youtube-background';
+export const iframe = {
+  iframes: document.querySelectorAll('.home-banner-iframe'),
+  init() {
+    const _ = this;
+    _.iframes.forEach((iframe) => {
+      if (!iframe) return;
+      new VideoBackgrounds(iframe);
+    });
+  },
+};
