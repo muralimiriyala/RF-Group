390ec2552d4947737fc355167be47f22c9a9adc9
MuraliJbk <murali@jbkinfotech.com>
2024-12-27 15:31:12 +0530

    test

--- html/js/modules/search.js
+++ html/js/modules/search.js
@@ -1,6 +1,11 @@
 export const search = {
-  ele: document.querySelector('header.main-header'),
+  form: document.querySelector('form.srch-form'),
+  btn: document.querySelector('.btn-srch'),
   init() {
     const _ = this;
+    _.btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      _.form.classList.toggle('open');
+    });
   },
 };
