7111da238bc4649486d5158eb34e100cc5b762c7
MuraliJbk <murali@jbkinfotech.com>
2025-01-02 16:02:21 +0530

    test

--- html/js/modules/menu.js
+++ html/js/modules/menu.js
@@ -1,6 +1,20 @@
 export const menu = {
-  ele: document.querySelector('header.main-header'),
+  $ele: document.querySelector('header.site-header'),
+  $nav: document.querySelector('.header_right'),
+  $btn: document.querySelector('.humburger-btn'),
+  $login: document.querySelector('.user-login'),
   init() {
     const _ = this;
+    _.$btn.addEventListener('click', (e) => {
+      const __ = e.currentTarget;
+      __.classList.toggle('open');
+      _.$nav.classList.toggle('open');
+    });
+
+    _.$login.addEventListener('click', (e) => {
+      const ___ = this;
+
+      console.log(___);
+    });
   },
 };
