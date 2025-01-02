48aaeb363b2796ac446e9b2b41d9c703debcfb80
MuraliJbk <murali@jbkinfotech.com>
2025-01-02 18:18:24 +0530

    test

--- html/js/core.js
+++ html/js/core.js
@@ -3,14 +3,14 @@
 import { jqueryselectBox } from './lib/jquery.selectBox.js';
 // import { DrawSVGPluginme } from './lib/DrawSVGPlugin.min.js';
 
-import { menu } from './modules/menu';
+import { menu } from './modules/menu.js';
 import { modal } from './modules/modal.js';
-import { search } from './modules/search';
+import { search } from './modules/search.js';
 import { iframe } from './modules/iframe.js';
-import { slick } from './modules/customSlick';
-import { share } from './modules/share';
-import { selectBox } from './modules/selectBox';
-import { magnificPopup } from './modules/magnificPopup';
+import { slick } from './modules/customSlick.js';
+import { share } from './modules/share.js';
+import { selectBox } from './modules/selectBox.js';
+import { magnificPopup } from './modules/magnificPopup.js';
 import { gsapanim } from './modules/gsap.js';
 import { gsapscroll } from './modules/gsapscroll.js';
 
