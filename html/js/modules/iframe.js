1ada15bc59d78ba79ccb7ae0651de091ba5c17e1
MuraliJbk <murali@jbkinfotech.com>
2025-01-04 11:49:35 +0530

    btn css

--- html/js/modules/iframe.js
+++ html/js/modules/iframe.js
@@ -1,9 +1,9 @@
-import $ from 'jquery';
 import 'youtube-background';
 export const iframe = {
   iframes: document.querySelectorAll('.home-banner-iframe'),
   init() {
     const _ = this;
+    if (!_.iframes) return;
     _.iframes.forEach((iframe) => {
       if (!iframe) return;
       new VideoBackgrounds(iframe);
