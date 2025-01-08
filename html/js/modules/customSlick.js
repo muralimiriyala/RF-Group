a2dd77d7beba7fc5c6c52ec1ac9c76620ae5c040
muralimiriyala <murali@jbkinfotech.com>
2025-01-08 21:45:46 +0530

    test

--- html/js/modules/customSlick.js
+++ html/js/modules/customSlick.js
@@ -2,17 +2,20 @@ import $ from 'jquery';
 import 'slick-slider';
 
 export const slick = {
-  slider: document.querySelectorAll('.slider'),
+  slider: document.querySelectorAll('.about-history-article'),
   init() {
     const _$ = this;
-    _$.slider.forEach((ele) => {
-      $(ele).slick({
-        dots: true,
-        infinite: false,
-        speed: 1000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+    if (window.matchMedia('(max-width:809px)').matches) {
+      _$.slider.forEach((ele) => {
+        $(ele).slick({
+          dots: true,
+          arrow: true,
+          infinite: false,
+          speed: 1000,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        });
       });
-    });
+    }
   },
 };
