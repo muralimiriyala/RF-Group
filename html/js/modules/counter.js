be0c9e36136a5c7c02d5db483878c40b5df0be19
muralimiriyala <murali@jbkinfotech.com>
2025-01-09 10:11:29 +0530

    test

--- html/js/modules/counter.js
+++ html/js/modules/counter.js
@@ -1,22 +1,32 @@
 import { CountUp } from 'countup.js';
+
 export const counter = {
   $ele: document.querySelectorAll('[data-count-to]'),
   init() {
     const _ = this;
     if (!_.$ele) return;
+
     _.$ele.forEach(($el) => {
       const $target = $el.getAttribute('id');
       const $duration = $el.getAttribute('data-duration');
       const $back = [...$el.parentElement.querySelectorAll('.counter-back')];
-      $el.counter = new CountUp(
-        `${$target}`,
-        $el.getAttribute('data-count-to'),
-        {
-          startVal: 0,
-          duration: parseInt(($el.getAttribute('data-duration') / 1000) * 1),
-        }
-      );
-      setTimeout(function () {
+
+      // Parse data-count-to as a number to check for decimals
+      const countTo = parseFloat($el.getAttribute('data-count-to'));
+      const decimalPlaces =
+        countTo % 1 !== 0 ? countTo.toString().split('.')[1].length : 0;
+
+      $el.counter = new CountUp(`${$target}`, countTo, {
+        startVal: 0,
+        duration: parseInt(($el.getAttribute('data-duration') / 1000) * 1),
+        decimalPlaces, // Ensure decimal precision
+      });
+
+      // Start the counter
+      $el.counter.start();
+
+      // Set opacity for the background elements after the duration
+      setTimeout(() => {
         $back.forEach(($ele) => {
           $ele.style.opacity = '1';
         });
