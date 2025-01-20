dcfa6c97bd3ad92ceca2afded58eb49d9fa0e457
muralimiriyala <murali@jbkinfotech.com>
2025-01-20 20:26:29 +0530

    test

--- html/js/modules/stickySidebar.js
+++ html/js/modules/stickySidebar.js
@@ -8,8 +8,12 @@ export const sticky = {
     if (!_.$sticky) return;
     const $height = _.$ele.getBoundingClientRect().height;
     _.$sticky.forEach((ele) => {
+      const $ptop = parseInt(
+        window.getComputedStyle(ele.parentElement.parentElement).paddingTop
+      );
+      console.log($ptop);
       new Sticksy(ele, {
-        topSpacing: $height,
+        topSpacing: $height + $ptop,
         listen: true,
       });
     });
