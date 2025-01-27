3901abd5aed3f58520f4ee762dccc6c866641634
MuraliJbk <murali@jbkinfotech.com>
2025-01-27 11:44:30 +0530

    test

--- html/js/modules/iframe.js
+++ html/js/modules/iframe.js
@@ -3,10 +3,11 @@ export const iframe = {
   iframes: document.querySelectorAll('.home-banner-iframe'),
   init() {
     const _ = this;
-
     _.iframes.forEach((iframe) => {
       if (!iframe) return;
-      new VideoBackgrounds(iframe);
+      new VideoBackgrounds(iframe, {
+        lazyloading: false,
+      });
     });
   },
 };
