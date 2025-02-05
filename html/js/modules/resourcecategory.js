12b8554e2d628c694dbd045949d941b1b380345e
MuraliJbk <murali@jbkinfotech.com>
2025-02-05 19:24:35 +0530

    test

---
+++ html/js/modules/resourcecategory.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+export const resourcecategory = {
+  $el: document.querySelector('ul.form-links'),
+  $btn: document.querySelector('.form-category-btn'),
+  init() {
+    const __ = this;
+    if (!__.$el) return;
+
+    if (window.matchMedia('(max-width: 809px)').matches) {
+      __.$el.addEventListener('click', function (e) {
+        if (e.target.tagName === 'A') {
+          e.target.classList.toggle('current');
+          __.$btn.classList.remove('current');
+          __.$btn.querySelector('span').textContent = e.target.textContent;
+          $(__.$el).slideUp(800);
+        }
+      });
+    }
+
+    const frmtoggle = (e) => {
+      e.currentTarget.classList.toggle('current');
+      $(__.$el).slideToggle(800);
+    };
+    if (!__.$btn) return;
+    __.$btn.addEventListener('click', frmtoggle);
+  },
+};
