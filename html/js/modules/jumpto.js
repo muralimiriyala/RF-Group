35158106a4b189667f4995b335f07a91e44359e3
MuraliJbk <murali@jbkinfotech.com>
2025-02-18 16:34:49 +0530

    test

--- html/js/modules/jumpto.js
+++ html/js/modules/jumpto.js
@@ -6,10 +6,34 @@ export const jumpto = {
     _.$jumptobtn.forEach(function (ele) {
       ele.addEventListener('click', function (e) {
         e.preventDefault();
-        e.target.classList.toggle('open');
-        let $links = e.target.parentElement.querySelector('ul.jumpto-links');
+        e.currentTarget.classList.toggle('open');
+        let $links =
+          e.currentTarget.parentElement.querySelector('ul.jumpto-links');
         $($links).slideToggle(700);
       });
+    });
+    const jumptoulMobile = () => {
+      _.$jumptoul.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (e.target.tagName === 'A') {
+          let $txt = e.target.textContent;
+          $(_.$jumptoul).slideUp(800);
+          _.$jumptobtn.forEach(function (ele) {
+            ele.classList.remove('open');
+            ele.querySelector('span').textContent = $txt;
+          });
+        }
+      });
+    };
+    let media = window.matchMedia('(max-width: 809px)');
+    if (media.matches) {
+      jumptoulMobile();
+    }
+    media.addEventListener('change', (event) => {
+      if (event.matches) {
+        jumptoulMobile();
+      } else {
+      }
     });
   },
 };
