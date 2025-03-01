63b5a8cffce817d80f2cec3b459db2e0c937d1b3
muralimiriyala <murali@jbkinfotech.com>
2025-03-01 14:01:31 +0530

    test number

---
+++ html/js/modules/inputNumber.js
@@ -0,0 +1,11 @@
+export const inputNumber = {
+  $ele: document.querySelectorAll('input[type="number"]'),
+  input() {
+    const _ = this;
+    _.$ele.forEach((ele) => {
+      ele.addEventListener('input', function (e) {
+        this.value = this.value.replace(/[^0-9.-]/g, '');
+      });
+    });
+  },
+};
