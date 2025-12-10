ce9c3d940be4f17aa7319ae922178d9e4e2e3107
ram lachu <ramlachu@jbkinfotech.com>
2025-12-10 15:48:37 +0530

    correction

--- html/js/modules/feeSchedule.js
+++ html/js/modules/feeSchedule.js
@@ -20,7 +20,7 @@ export const feeSchedule = {
         if (row) {
           $(row).fadeIn(800);
         }
-        if (window.matchMedia('(min-width: 759px)').matches) {
+        if (window.matchMedia('(min-width: 760px)').matches) {
           __.$btn.classList.remove('current');
           $(__.$el).slideUp(800);
           const $text = e.target.textContent;
@@ -29,11 +29,13 @@ export const feeSchedule = {
       }
     };
     if (!__.$el) return;
+    if (window.matchMedia('(max-width: 759px)').matches) {
     __.$el.addEventListener('click', toggle);
     const mbltoggle = (e) => {
       e.currentTarget.classList.toggle('current');
       $(__.$el).slideToggle(800);
     };
     __.$btn.addEventListener('click', mbltoggle);
+  }
   },
 };
